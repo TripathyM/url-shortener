a4b23a5fa8df49d86462e25f01abd5af5e4008fb
Manish Tripathy <mtripathy@palo-it.com>
2024-04-02 21:03:12 +0800

    Implement LinkService#createShortLink

--- backend/src/link/link.service.ts
+++ backend/src/link/link.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { CreateLinkRequest } from './link.dto';
+import { CreateLinkRequest, CreateLinkResponse } from './link.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Link } from './link.entity';
+import { Repository } from 'typeorm';
+import { EncoderUtils } from '../utils/encoder.utilty';
 
 @Injectable()
 export class LinkService {
-  async createShortLink(request: CreateLinkRequest) {
-    throw new Error('Method not implemented.');
+  constructor(
+    @InjectRepository(Link) private readonly linkRepository: Repository<Link>,
+  ) {}
+
+  async createShortLink({
+    actualUrl,
+  }: CreateLinkRequest): Promise<CreateLinkResponse> {
+    const { identifiers } = await this.linkRepository.insert({
+      actualUrl,
+      slug: 'PENDING', // This is an intermediate value and will be updated to a unique slug later.
+    });
+    const insertedRecordId: number = identifiers[0].id;
+    const slug = EncoderUtils.encode(insertedRecordId);
+    await this.linkRepository.update(insertedRecordId, {
+      slug,
+    });
+
+    return {
+      actualUrl,
+      shortUrl: `${process.env.BASE_URL}/${slug}`,
+    };
   }
 }
