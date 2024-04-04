3eef0f9e17942029e72592c48f3d256eb4c73331
Manish Tripathy <mtripathy@palo-it.com>
2024-04-04 20:53:48 +0800

    Use a larger prime number for randomization

--- backend/src/utils/encoder.utilty.ts
+++ backend/src/utils/encoder.utilty.ts
@@ -3,7 +3,7 @@ export class EncoderUtils {
   private static readonly MAX_BASE62_FOUR_CHAR_VALUE = 14776335; // We are targeting a 5 chars length slug. 62^4 is the highest decimal which can be represent using 5 base62 chars. Hence this number is 62^4-1
   private static readonly BASE62_CHARSET =
     'kbHfZuE1XjOA2Dx06qd4PaVzJBSrNQWg3FU5MmnYsv7etClRTLycIopwGi8Kh9'; // This has been jumbled to make avoid sequential slugs
-  private static readonly PRIME_NUMBER = 101; // This is used to make the encoded number randomized
+  private static readonly PRIME_NUMBER = 1087; // This is used to make the encoded number randomized
 
   static encode(numberToEncode: number): string {
     let adjustedNumber =
